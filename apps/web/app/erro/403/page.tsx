8b07ad9d5459eea35eb5db943a3931926a4c51e7
Matheus Gois <matheusmestriner22@gmail.com>
2026-09-17 16:25:54 -0300

    Add branded error pages

---
+++ apps/web/app/erro/403/page.tsx
@@ -0,0 +1,3 @@
+import ErrorPage from '../../ui/error-page';
+export default function Page(){return <ErrorPage code="403" title="Acesso não autorizado" message="Sua conta não tem permissão para acessar este conteúdo."/>}
+
