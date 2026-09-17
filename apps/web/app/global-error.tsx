366b1d3ef1f07809c6157126c714ae4fa97da668
Matheus Gois <matheusmestriner22@gmail.com>
2026-09-17 16:25:52 -0300

    Add branded error pages

---
+++ apps/web/app/global-error.tsx
@@ -0,0 +1,4 @@
+'use client';
+import ErrorPage from './ui/error-page';
+export default function GlobalError(){return <html lang="pt-BR"><body><ErrorPage code="500" title="A plataforma precisa de um instante" message="Não foi possível concluir o carregamento. Tente novamente em alguns instantes." retry/></body></html>}
+
