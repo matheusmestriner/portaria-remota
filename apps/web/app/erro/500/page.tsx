c1f7a04ed56455c68d232c92e207416f5d1ca706
Matheus Gois <matheusmestriner22@gmail.com>
2026-09-17 16:25:59 -0300

    Add branded error pages

---
+++ apps/web/app/erro/500/page.tsx
@@ -0,0 +1,3 @@
+import ErrorPage from '../../ui/error-page';
+export default function Page(){return <ErrorPage code="500" title="Algo deu errado" message="O serviço encontrou um problema. Tente novamente em alguns instantes." retry/>}
+
