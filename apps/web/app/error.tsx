d29dbd8d9a488c4b606b7dbb4e95066537e65871
Matheus Gois <matheusmestriner22@gmail.com>
2026-09-17 16:25:50 -0300

    Add branded error pages

---
+++ apps/web/app/error.tsx
@@ -0,0 +1,4 @@
+'use client';
+import ErrorPage from './ui/error-page';
+export default function Error(){return <ErrorPage code="500" title="Não foi possível carregar esta página" message="Ocorreu um erro inesperado. Tente novamente em alguns instantes." retry/>}
+
