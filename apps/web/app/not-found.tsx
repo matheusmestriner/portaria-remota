5b0d83e2c3c1d8a19ed7b0b29765a98fdb866495
Matheus Gois <matheusmestriner22@gmail.com>
2026-09-17 16:25:48 -0300

    Add branded error pages

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,3 @@
+import ErrorPage from './ui/error-page';
+export default function NotFound(){return <ErrorPage code="404" title="Página não encontrada" message="O endereço pode ter mudado ou não existe mais."/>}
+
