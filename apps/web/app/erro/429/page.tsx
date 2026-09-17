0a3a4998f69575d769054f5ece390e6312d26ee0
Matheus Gois <matheusmestriner22@gmail.com>
2026-09-17 16:25:57 -0300

    Add branded error pages

---
+++ apps/web/app/erro/429/page.tsx
@@ -0,0 +1,3 @@
+import ErrorPage from '../../ui/error-page';
+export default function Page(){return <ErrorPage code="429" title="Muitas tentativas" message="Reduzimos temporariamente o acesso para proteger sua conta. Aguarde um pouco e tente novamente." retry/>}
+
